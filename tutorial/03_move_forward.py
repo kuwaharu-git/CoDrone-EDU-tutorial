25fc8f0e029dc04a7614aede674069e72f868165
kuwaharu-git <kuwaharu6@gmail.com>
2024-10-11 11:47:57 +0900

    移動のプログラムを修正

--- tutorial/03_move_forward.py
+++ tutorial/03_move_forward.py
@@ -11,22 +11,34 @@ drone.pair()
 
 print("離陸")
 drone.takeoff()
-print("1秒間ホバリング")
-drone.hover(1)
+
+print("3秒間ホバリング")
+drone.hover(3)
+
 # ドローンを前進させる
 print("ピッチ変数を30に設定します")
 drone.set_pitch(30)  # ピッチ変数の値を30に設定
 print("3秒間前進")
 drone.move(3)  # 3秒間前進
-print("1秒間ホバリング")
-drone.hover(1)
+
+# ドローンの飛行変数の値を表示
+print("ドローンの飛行変数の値を表示します")
+drone.print_move_values()
+
+print("3秒間ホバリング")
+drone.hover(3)
+
 print("再度3秒間前進")
 drone.move(3)  # 再度3秒間前進
-drone.hover(1)
+
+print("3秒間ホバリング")
+drone.hover(3)
+
 print("ピッチ変数を0に設定します")
 drone.set_pitch(0)  # ピッチ変数の値を0に設定
-print("3秒間ホバリング")
-drone.move(3)  # 今度は動かない(3秒間ホバリング)
+print("move(3)を実行します")
+drone.move(3)  # 今度は動かない
+
 drone.land()
 
 drone.close()
